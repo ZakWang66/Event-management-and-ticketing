3b3bfcdd8800ea52e251b16f6048ddda965304b4
Dongyu Zhao <danielzhaowise@gmail.com>
2020-03-24 01:39:09 -0400

    feat: update edit password ui

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'sign_in', to: 'session#new'
   post 'log_in', to: 'session#create'
   patch 'users/:id/update_portrait', to: 'users#updatePortrait', as: 'update_portrait'
+  patch 'users/:id/update_pwd', to: 'users#updatePassword', as: 'update_pwd'
   get 'profile', to: 'profile#show'
   get 'api/get_events', to: 'api#getEvents', as: 'get_events'
   get 'events', to: 'events#index'
