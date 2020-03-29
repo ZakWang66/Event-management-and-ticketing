3bf07ccfbcd06ee4df1666ba901a8bf50341e481
axeldong <axeldong3@gmail.com>
2020-03-29 00:02:06 -0400

    gallery functions

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,4 +12,16 @@ class Event < ApplicationRecord
     scope :with_co_organizers, -> { with_roles(Participant.co_organizer) }
     scope :with_audience, -> { with_roles(Participant.audience) }
     scope :with_visitors, -> { with_roles(Participant.visitor) }
+    
+    scope :filter_by_price, -> (prices) {where("price > ? AND price <= ?", prices[0], prices[1])}
+
+    def self.order_list(sort_order)
+        if sort_order == "price"
+            order(price: :desc)
+        elsif sort_order == "date"
+            order(time: :desc)
+        else
+            order(created_at: :asc)
+        end
+    end
 end
