0a6ed1856c9fae90eb444b6c05c34185f0a8cfaf
Dongyu Zhao <danielzhaowise@gmail.com>
2020-03-30 14:56:42 -0400

    feat: add follows

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,5 +63,5 @@ class PostsController < ApplicationController
     def correct_user
         @post = current_user.posts.find_by(id: params[:id])
         redirect_to root_url if @post.nil?
-      end
+    end
 end
---
+++ test/controllers/follows_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class FollowsControllerTest < ActionDispatch::IntegrationTest
+  test "should get follow" do
+    get follows_follow_url
+    assert_response :success
+  end
+
+end
