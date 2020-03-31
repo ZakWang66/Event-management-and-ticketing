8fe77d2cd20ae9bbdaad3038cb2273344450b289
zakwang66 <zakwang66@gmail.com>
2020-03-31 14:37:22 -0400

    test 2

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
-  test "should get googleAuth" do
-    get session_googleAuth_url
-    assert_response :success
-  end
-
-  test "should get signOut" do
-    get session_signOut_url
-    assert_response :success
-  end
-
-  test "should get signIn" do
-    get session_signIn_url
-    assert_response :success
-  end
-
+  
 end
