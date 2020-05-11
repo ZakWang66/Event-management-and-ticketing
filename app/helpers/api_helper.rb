260d07ccdc3cb40a15ea63f93efce9051d6ee850
Dongyu Zhao <danielzhaowise@gmail.com>
2020-05-11 11:40:08 -0400

    feat: optimize login sign up and events ui

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -5,39 +5,47 @@ module ApiHelper
             tag += "Empty Event"
         else
             participants.each do |participant|
-                get_bg_color(participant.role)
-                event = participant.event
-                tag += "
-                <div class=\"card #{get_bg_color(participant.role)}\">
-                    <div class=\"card-body\">
-                        <h4 class=\"card-title text-center\">#{event.title}</h4>
-                        <p class=\"card-text\">Place: #{event.place}</p>
-                        <p class=\"card-text\">Time: #{event.time}</p>
-                        <p class=\"card-text\">Role: #{participant.role}</p>
-                        <form action=\"/events/#{event[:id]}\" method=\"get\">
-                            <button type=\"submit\" class=\"btn btn-warning\">View</button>
-                        </form>
-                    </div>
-                </div>"
+                tag += load_event(participant)
             end
         end
         return tag.html_safe
     end
 
-    def load_event(event)
-    end
-
     private
-    def get_bg_color(role)
+    def get_text_color(role)
         case Participant.roles[role]
         when 1 #orginizer
-            "bg-danger"
+            "text-danger"
         when 2 #co_orginizer
-            "bg-primary"
+            "text-primary"
         when 3 #audience
-            "bg-success"
+            "text-success"
         when 0 #visitor
-            "bg-secondary"
+            "text-secondary"
+        end
+    end
+
+    def load_event(participant)
+        event = participant.event
+        content_tag(:div, nil, class: "card bg-light mb-3") do
+            link_to(event_path(id: event.id), class: "nounderline") do
+                content_tag(:div, nil, class: "card-body") do
+                    content_tag(:h4, event.title, class: "card-title text-center") +
+                    content_tag(:hr, nil) + 
+                    content_tag(:div, nil, class: "hover_img text-center") do
+                        if Picture.where(event_id: event[:id]).empty?
+                            image_pack_tag("poster#{event[:id] % 30 + 1}.jpg", height: "100px")
+                        else
+                            image_tag(Picture.where(event_id: event[:id]).first.picture.url, height: "100px")
+                        end
+                    end +
+                    content_tag(:div, nil, class: "row") do
+                        content_tag(:div, "Place: #{event.place}", class: "card-text col-4") +
+                        content_tag(:div, "Time: #{event.time}", class: "card-text col-4") +
+                        content_tag(:div, "Role: #{participant.role}", class: "card-text #{get_text_color participant.role} col-4")
+                    end
+                end
+            end
         end
     end
 end
